18a733ea8abad2f06a5fe388b4e29daca409b732
Afreen Khatoon <afreen.khatoon@Mohammads-MacBook-Pro.local>
2025-08-30 18:20:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Dashboard from "./pages/Dashboard";
+const App = () => {
+  return (
+    <div className='text-red-300 text-6xl'>
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default App
